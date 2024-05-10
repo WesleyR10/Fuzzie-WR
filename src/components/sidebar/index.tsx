ed81ba03cb3a2c1a479dc5590aed28e98fdc6c60
WesleyR10 <wesleyribas2015@gmail.com>
2024-05-10 19:09:37 -0300

    Toogle Mode and Infobar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -12,7 +12,7 @@ import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
 import { Separator } from '@/components/ui/separator'
 import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
-// import { ModeToggle } from '../global/mode-toggle'
+import { ModeToggle } from '../global/mode-toggle'
 
 type Props = {}
 
@@ -92,7 +92,7 @@ const MenuOptions = (props: Props) => {
         </div>
       </div>
       <div className="flex items-center justify-center flex-col gap-8">
-        {/* <ModeToggle /> */}
+        <ModeToggle />
       </div>
     </nav>
   )
