db3ba6439dd21a08776563ac78e739f9ed7b0474
WesleyR10 <wesleyribas2015@gmail.com>
2024-05-12 19:13:48 -0300

    Finish project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/providers/theme-provider"
+import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/providers/theme-provider'
+import { ClerkProvider } from '@clerk/nextjs'
+import ModalProvider from '@/providers/modal-provider'
+import { Toaster } from '@/components/ui/sonner'
+import { BillingProvider } from '@/providers/billing-provider'
 
 const font = DM_Sans({ subsets: ['latin'] })
 
@@ -14,20 +17,29 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <BillingProvider>
+              <ModalProvider>
+                {children}
+                <Toaster />
+              </ModalProvider>
+            </BillingProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
